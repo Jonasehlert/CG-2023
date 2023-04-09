fc14e983248ba3d79cd8d7f019824b58cff5b53e
Jonas Ehlert <jdaehlert@gmail.com>
2023-04-09 16:31:08 -0300

    UP2

--- Hello3D - Mesh - POA/Hello3D - Pyramid/Origem.cpp
+++ Hello3D - Mesh - POA/Hello3D - Pyramid/Origem.cpp
@@ -10,9 +10,9 @@
 #include <iostream>
 #include <string>
 #include <assert.h>
-#include <vector>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -27,64 +27,41 @@ using namespace std;
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include "Shader.h"
+
+
 struct Vertex
 {
 	glm::vec3 position;
-	glm::vec3 color;
+	glm::vec3 v_color;
 };
-
 
 // Protótipo da função de callback de teclado
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
-// Protótipo da função de callback do mouse
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 
-
 // Protótipos das funções
-int setupShader();
 int setupGeometry();
-int loadSimpleOBJ(string filepath, int &nVerts);
+int loadSimpleObj(string filePath, int& nVertices);
+
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 1000, HEIGHT = 1000;
 
-// Código fonte do Vertex Shader (em GLSL): ainda hardcoded
-const GLchar* vertexShaderSource = "#version 450\n"
-"layout (location = 0) in vec3 position;\n"
-"layout (location = 1) in vec3 color;\n"
-"uniform mat4 model;\n"
-"uniform mat4 view;\n"
-"uniform mat4 projection;\n"
-"out vec4 finalColor;\n"
-"void main()\n"
-"{\n"
-//...pode ter mais linhas de código aqui!
-"gl_Position = projection * view * model * vec4(position, 1.0);\n"
-"finalColor = vec4(color, 1.0);\n"
-"}\0";
-
-//Códifo fonte do Fragment Shader (em GLSL): ainda hardcoded
-const GLchar* fragmentShaderSource = "#version 450\n"
-"in vec4 finalColor;\n"
-"out vec4 color;\n"
-"void main()\n"
-"{\n"
-"color = finalColor;\n"
-"}\n\0";
-
-bool rotateX=false, rotateY=false, rotateZ=false;
+bool rotateX = false, rotateY = false, rotateZ = false;
 
 glm::vec3 cameraPos = glm::vec3(0.0, 0.0, 3.0);
 glm::vec3 cameraFront = glm::vec3(0.0, 0.0, -1.0);
 glm::vec3 cameraUp = glm::vec3(0.0, 1.0, 0.0);
+float cameraSpeed = 0.05;
 
 bool firstMouse = true;
-float lastX, lastY;
-float sensitivity = 0.05;
-float pitch = 0.0, yaw = -90.0;
+float lastX = 0.0, lastY = 0.0;
+float yaw = -90.0, pitch = 0.0;
 
 vector <Vertex> vertices;
-vector <GLuint> indices;
+vector <int> indices;
+
 
 // Função MAIN
 int main()
@@ -106,20 +83,17 @@ int main()
 //#endif
 
 	// Criação da janela GLFW
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola 3D!", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ola Triangulo!", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
 	// Fazendo o registro da função de callback para a janela GLFW
 	glfwSetKeyCallback(window, key_callback);
+	//glfwSetCursorPos(window, WIDTH / 2, HEIGHT / 2);
 	glfwSetCursorPosCallback(window, mouse_callback);
-	
-
-	glfwSetCursorPos(window,WIDTH / 2, HEIGHT / 2);
 
 
-	//Desabilita o desenho do cursor 
+	//Desabilita o desenho do cursor do mouse
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
 
 	// GLAD: carrega todos os ponteiros d funções da OpenGL
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -141,34 +115,32 @@ int main()
 
 
 	// Compilando e buildando o programa de shader
-	GLuint shaderID = setupShader();
+	Shader shader("Hello3D.vs", "Hello3D.fs");
 
 	// Gerando um buffer simples, com a geometria de um triângulo
-	GLuint VAO = setupGeometry();
+	int nVertices;
+	GLuint VAO = loadSimpleObj("../../3D_Models/Classic-NoTexture/bunny.obj", nVertices);
 
+	glUseProgram(shader.ID);
 
-	glUseProgram(shaderID);
-
-	// Matriz de model -- transformações no objeto
 	glm::mat4 model = glm::mat4(1); //matriz identidade;
-	GLint modelLoc = glGetUniformLocation(shaderID, "model");
+	GLint modelLoc = glGetUniformLocation(shader.ID, "model");
+	//
 	model = glm::rotate(model, /*(GLfloat)glfwGetTime()*/glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	glUniformMatrix4fv(modelLoc, 1, FALSE, glm::value_ptr(model));
 
-	//Matriz de view -- posição e orientação da câmera
+	//Definindo a matriz de view (posição e orientação da câmera)
 	glm::mat4 view = glm::lookAt(glm::vec3(0.0, 0.0, 3.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
-	GLint viewLoc = glGetUniformLocation(shaderID, "view");
+	GLint viewLoc = glGetUniformLocation(shader.ID, "view");
 	glUniformMatrix4fv(viewLoc, 1, FALSE, glm::value_ptr(view));
 
-	//Matriz de projeção perspectiva - definindo o volume de visualização (frustum)
-	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
-	GLint projLoc = glGetUniformLocation(shaderID, "projection");
+	//Definindo a matriz de projeção perpectiva
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+	GLint projLoc = glGetUniformLocation(shader.ID, "projection");
 	glUniformMatrix4fv(projLoc, 1, FALSE, glm::value_ptr(projection));
 
 	glEnable(GL_DEPTH_TEST);
 
-	int nVerts;
-	GLuint VAO2 = loadSimpleOBJ("../suzanne.obj",nVerts);
 
 	// Loop da aplicação - "game loop"
 	while (!glfwWindowShouldClose(window))
@@ -185,15 +157,13 @@ int main()
 
 		float angle = (GLfloat)glfwGetTime();
 
-		model = glm::mat4(1); 
+		model = glm::mat4(1);
 
-		// model = glm::translate(model, glm::vec3(0.0, 0.0, cos(angle)*10.0));
-
-
+		// model = glm::translate(model, glm::vec3(0.0, 0.0, cos(angle) * 10.0));
 		if (rotateX)
 		{
 			model = glm::rotate(model, angle, glm::vec3(1.0f, 0.0f, 0.0f));
-			
+
 		}
 		else if (rotateY)
 		{
@@ -207,21 +177,23 @@ int main()
 		}
 
 		glUniformMatrix4fv(modelLoc, 1, FALSE, glm::value_ptr(model));
-		
-		//Atualizando a posição e orientação da câmera
+
+		//Alterando a matriz de view (posição e orientação da câmera)
 		glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 		glUniformMatrix4fv(viewLoc, 1, FALSE, glm::value_ptr(view));
-		
+
+
+
 		// Chamada de desenho - drawcall
 		// Poligono Preenchido - GL_TRIANGLES
-		
-		glBindVertexArray(VAO2);
-		glDrawArrays(GL_TRIANGLES, 0, nVerts);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, nVertices);
 
 		// Chamada de desenho - drawcall
 		// CONTORNO - GL_LINE_LOOP
-		
-		glDrawArrays(GL_POINTS, 0, nVerts);
+
+		glDrawArrays(GL_POINTS, 0, nVertices);
 		glBindVertexArray(0);
 
 		// Troca os buffers da tela
@@ -263,20 +235,21 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		rotateZ = true;
 	}
 
-	float cameraSpeed = 0.05;
-
 	if (key == GLFW_KEY_W)
 	{
-		cameraPos += cameraFront * cameraSpeed;
+		cameraPos += cameraSpeed * cameraFront;
 	}
+
 	if (key == GLFW_KEY_S)
 	{
-		cameraPos -= cameraFront * cameraSpeed;
+		cameraPos -= cameraSpeed * cameraFront;
 	}
+
 	if (key == GLFW_KEY_A)
 	{
 		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 	}
+
 	if (key == GLFW_KEY_D)
 	{
 		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
@@ -288,7 +261,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	//cout << xpos << " " << ypos << endl;
+
+	// cout << xpos << " " << ypos << endl;
+
 	if (firstMouse)
 	{
 		lastX = xpos;
@@ -298,12 +273,11 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 	float offsetx = xpos - lastX;
 	float offsety = lastY - ypos;
-
 	lastX = xpos;
 	lastY = ypos;
 
-	offsetx *= sensitivity;
-	offsety *= sensitivity;
+	offsetx *= cameraSpeed;
+	offsety *= cameraSpeed;
 
 	pitch += offsety;
 	yaw += offsetx;
@@ -316,53 +290,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 }
 
-//Esta função está basntante hardcoded - objetivo é compilar e "buildar" um programa de
-// shader simples e único neste exemplo de código
-// O código fonte do vertex e fragment shader está nos arrays vertexShaderSource e
-// fragmentShader source no iniçio deste arquivo
-// A função retorna o identificador do programa de shader
-int setupShader()
-{
-	// Vertex shader
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-	// Checando erros de compilação (exibição via log no terminal)
-	GLint success;
-	GLchar infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	// Fragment shader
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-	// Checando erros de compilação (exibição via log no terminal)
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-	}
-	// Linkando os shaders e criando o identificador do programa de shader
-	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	// Checando por erros de linkagem
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
-	return shaderProgram;
-}
 
 // Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a 
 // geometria de um triângulo
@@ -387,37 +314,34 @@ int setupGeometry()
 		  0.5, -0.5,  0.5, 0.0, 1.0, 1.0,
 		  0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
 
-		 //
-		 -0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
-		  0.0,  0.5,  0.0, 1.0, 1.0, 0.0,
-		  0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
+		  //
+		  -0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
+		   0.0,  0.5,  0.0, 1.0, 1.0, 0.0,
+		   0.5, -0.5, -0.5, 1.0, 1.0, 0.0,
 
-		  -0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
-		  0.0,  0.5,  0.0, 1.0, 0.0, 1.0,
-		  -0.5, -0.5, 0.5, 1.0, 0.0, 1.0,
+		   -0.5, -0.5, -0.5, 1.0, 0.0, 1.0,
+		   0.0,  0.5,  0.0, 1.0, 0.0, 1.0,
+		   -0.5, -0.5, 0.5, 1.0, 0.0, 1.0,
 
-		   -0.5, -0.5, 0.5, 1.0, 1.0, 0.0,
-		  0.0,  0.5,  0.0, 1.0, 1.0, 0.0,
-		  0.5, -0.5, 0.5, 1.0, 1.0, 0.0,
+			-0.5, -0.5, 0.5, 1.0, 1.0, 0.0,
+		   0.0,  0.5,  0.0, 1.0, 1.0, 0.0,
+		   0.5, -0.5, 0.5, 1.0, 1.0, 0.0,
 
-		   0.5, -0.5, 0.5, 0.0, 1.0, 1.0,
-		  0.0,  0.5,  0.0, 0.0, 1.0, 1.0,
-		  0.5, -0.5, -0.5, 0.0, 1.0, 1.0,
+			0.5, -0.5, 0.5, 0.0, 1.0, 1.0,
+		   0.0,  0.5,  0.0, 0.0, 1.0, 1.0,
+		   0.5, -0.5, -0.5, 0.0, 1.0, 1.0,
 
-		  //Chão
-		  //x    y    z    r    g    b
-		  -5.0, -0.5, -5.0, 0.5, 0.5, 0.5,
-		  -5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
-		   5.0, -0.5, -5.0, 0.5, 0.5, 0.5,
+		   //"Chão"
+		   -5.0, -0.5, -5.0, 0.5, 0.5, 0.5,
+		   -5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
+			5.0, -0.5, -5.0, 0.5, 0.5, 0.5,
 
-		  -5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
-		   5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
-		   5.0, -0.5, -5.0, 0.5, 0.5, 0.5
+		   -5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
+			5.0, -0.5,  5.0, 0.5, 0.5, 0.5,
+			5.0, -0.5, -5.0, 0.5, 0.5, 0.5
+
 
 	};
-
-	nVertices = vertbuffer.size() / 6;
-
 
 	GLuint VBO, VAO;
 
@@ -436,7 +360,7 @@ int setupGeometry()
 	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
 	// e os ponteiros para os atributos 
 	glBindVertexArray(VAO);
-	
+
 	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
 	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
 	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
@@ -444,13 +368,13 @@ int setupGeometry()
 	// Se está normalizado (entre zero e um)
 	// Tamanho em bytes 
 	// Deslocamento a partir do byte zero 
-	
+
 	//Atributo posição (x, y, z)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
 	glEnableVertexAttribArray(0);
 
 	//Atributo cor (r, g, b)
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3*sizeof(GLfloat)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 	glEnableVertexAttribArray(1);
 
 
@@ -465,73 +389,68 @@ int setupGeometry()
 	return VAO;
 }
 
-int loadSimpleOBJ(string filepath, int &nVerts)
+int loadSimpleObj(string filePath, int& nVertices)
 {
-	vector <GLfloat> vbuffer;
 	ifstream inputFile;
-	inputFile.open(filepath.c_str());
+	inputFile.open(filePath);
+	vector <GLfloat> vertbuffer;
+
 	if (inputFile.is_open())
 	{
 		char line[100];
 		string sline;
-		
-		
-		
+
+
 		while (!inputFile.eof())
 		{
 			inputFile.getline(line, 100);
 			sline = line;
-			
+
 			string word;
-			
-			istringstream ssline(line);
+			istringstream ssline(sline);
+
 			ssline >> word;
 
-			//cout << word << " ";
 			if (word == "v")
 			{
 				Vertex v;
-
 				ssline >> v.position.x >> v.position.y >> v.position.z;
-				v.color.r = 1.0;  v.color.g = 0.0;  v.color.b = 0.0;
-				
+				v.v_color.r = 1.0; v.v_color.g = 0.0; v.v_color.b = 0.0;
 				vertices.push_back(v);
 			}
-			if (word == "f")
+			else if (word == "f")
 			{
 				string tokens[3];
-				
-				ssline >> tokens[0] >> tokens[1] >> tokens[2];
-				
 				for (int i = 0; i < 3; i++)
 				{
+					ssline >> tokens[i];
 					int pos = tokens[i].find("/");
 					string token = tokens[i].substr(0, pos);
-					//cout << token << endl;
 					int index = atoi(token.c_str()) - 1;
-					cout << vertices[index].position.x << "  " << vertices[index].position.y << " " << vertices[index].position.z << endl;
-					vbuffer.push_back(vertices[index].position.x);
-					vbuffer.push_back(vertices[index].position.y);
-					vbuffer.push_back(vertices[index].position.z);
-					vbuffer.push_back(vertices[index].color.r);
-					vbuffer.push_back(vertices[index].color.g);
-					vbuffer.push_back(vertices[index].color.b);
-				
+					indices.push_back(index);
+					vertbuffer.push_back(vertices[index].position.x);
+					vertbuffer.push_back(vertices[index].position.y);
+					vertbuffer.push_back(vertices[index].position.z);
+					vertbuffer.push_back(vertices[index].v_color.r);
+					vertbuffer.push_back(vertices[index].v_color.g);
+					vertbuffer.push_back(vertices[index].v_color.b);
 				}
+
 			}
 
 		}
 
+		inputFile.close();
 	}
 	else
 	{
-		cout << "Problema ao encontrar o arquivo " << filepath << endl;
+		cout << "Não foi possivel abrir o arquivo " << filePath << endl;
 	}
-	inputFile.close();
+
+
+	nVertices = vertbuffer.size() / 6;
 
 	GLuint VBO, VAO;
-
-	nVerts = vbuffer.size() / 6;
 
 	//Geração do identificador do VBO
 	glGenBuffers(1, &VBO);
@@ -540,7 +459,7 @@ int loadSimpleOBJ(string filepath, int &nVerts)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
 	//Envia os dados do array de floats para o buffer da OpenGl
-	glBufferData(GL_ARRAY_BUFFER, vbuffer.size() * sizeof(GLfloat), vbuffer.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, vertbuffer.size() * sizeof(GLfloat), vertbuffer.data(), GL_STATIC_DRAW);
 
 	//Geração do identificador do VAO (Vertex Array Object)
 	glGenVertexArrays(1, &VAO);
@@ -577,4 +496,3 @@ int loadSimpleOBJ(string filepath, int &nVerts)
 	return VAO;
 
 }
-
